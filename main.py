bef07582f5ed88c3bd7e12627093e6429354c1c5
Sangameeee <137791865+Sangameeee@users.noreply.github.com>
2024-05-14 08:04:09 +0545

    created database
    Used sqlalchemy to create database to store form submitted data

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,TextAreaField
 from wtforms.validators import DataRequired 
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 class ContactForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
@@ -11,18 +14,38 @@ class ContactForm(FlaskForm):
 
 app = Flask(__name__)
 app.secret_key = "dontbelikethat"
+app.config['SQLALCHEMY_DATABASE_URI'] =  "sqlite:///user-message.db"
+db = SQLAlchemy(app)
 
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    email = db.Column(db.String(25))
+    message = db.Column(db.String(1000))
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/contactme')
+@app.route('/contactme', methods=['GET','POST'])
 def contact_page():
     contact_form = ContactForm()
-    contact_form.validate_on_submit()
-    return render_template('contact.html', form=contact_form) 
+   
+    if request.method == "POST" and contact_form.validate_on_submit():
+        name = contact_form.name.data
+        email = contact_form.email.data
+        message = contact_form.message.data
 
+        new_message = Message(name=name, email=email, message=message)
+        db.session.add(new_message)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('contact.html', form=contact_form) 
 
 
 if __name__ == "__main__":
