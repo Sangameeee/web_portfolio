33352fb0af0a18d923de6cb174ef887171759e12
Sangameeee <137791865+Sangameeee@users.noreply.github.com>
2024-06-07 19:52:11 +0545

    created admin
    inserted small admin page to read and delete messages

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash, session
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,TextAreaField
-from wtforms.validators import DataRequired 
+from wtforms import StringField, SubmitField, TextAreaField, PasswordField
+from wtforms.validators import DataRequired, Email
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 import os
 from dotenv import load_dotenv
 
-
 class ContactForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email')
+    email = StringField('Email', validators=[Email()])
     message = TextAreaField('Message', validators=[DataRequired()])
-    submit  = SubmitField("Submit")
+    submit = SubmitField("Submit")
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
 
 load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
-
+# Admin credentials
+ADMIN_USERNAME = os.getenv('ADMIN_USERNAME')
+ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
+ADMIN_PASSWORD_HASH = bcrypt.generate_password_hash(ADMIN_PASSWORD).decode('utf-8')
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250))
-    email = db.Column(db.String(25))
+    email = db.Column(db.String(250))
     message = db.Column(db.String(1000))
 
 with app.app_context():
@@ -34,10 +43,9 @@ with app.app_context():
 def home():
     return render_template('index.html')
 
-@app.route('/contactme', methods=['GET','POST'])
+@app.route('/contactme', methods=['GET', 'POST'])
 def contact_page():
     contact_form = ContactForm()
-   
     if request.method == "POST" and contact_form.validate_on_submit():
         name = contact_form.name.data
         email = contact_form.email.data
@@ -47,9 +55,45 @@ def contact_page():
         db.session.add(new_message)
         db.session.commit()
         return redirect(url_for('home'))
-    return render_template('contact.html', form=contact_form) 
+    return render_template('contact.html', form=contact_form)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        if username == ADMIN_USERNAME and bcrypt.check_password_hash(ADMIN_PASSWORD_HASH, password):
+            session['admin_logged_in'] = True
+            return redirect(url_for('admin_page'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/admin', methods=['GET'])
+def admin_page():
+    if not session.get('admin_logged_in'):
+        return redirect(url_for('login'))
+    messages = Message.query.all()
+    return render_template('admin.html', messages=messages)
+
+@app.route('/delete_message/<int:message_id>', methods=['POST'])
+def delete_message(message_id):
+    if not session.get('admin_logged_in'):
+        return redirect(url_for('login'))
+    message = Message.query.get(message_id)
+    if message:
+        db.session.delete(message)
+        db.session.commit()
+        flash('Message deleted successfully', 'success')
+    else:
+        flash('Message not found', 'error')
+    return redirect(url_for('admin_page'))
+
+@app.route('/logout')
+def logout():
+    session.pop('admin_logged_in', None)
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)
-
